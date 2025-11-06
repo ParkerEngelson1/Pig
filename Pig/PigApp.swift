e5b2b8e05ff30106f2516c242e3a8c378b91c20d
Parker <Parker.engelson09@gmail.com>
2025-11-05 19:10:58 -0800

    Initial Commit

---
+++ Pig/PigApp.swift
@@ -0,0 +1,17 @@
+//
+//  PigApp.swift
+//  Pig
+//
+//  Created by Parker Engelson on 11/5/25.
+//
+
+import SwiftUI
+
+@main
+struct PigApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

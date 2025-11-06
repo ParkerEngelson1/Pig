a0f85b58785232e9337e9e26ff7479ac69bad50a
Parker <Parker.engelson09@gmail.com>
2025-11-06 09:18:56 -0800

    Finished game mechanics

--- Pig/ContentView.swift
+++ Pig/ContentView.swift
@@ -12,6 +12,7 @@ struct ContentView: View {
     @State var gameScore = 0
     @State private var randomValue = 0
     @State private var rotation = 0.0
+    @State private var gameOver = false
     var body: some View {
         NavigationView{
             ZStack {
@@ -41,15 +42,31 @@ struct ContentView: View {
                             withAnimation(.easeInOut(duration: 1)){
                                 rotation += 360
                             }
+                            if gameScore >= 100 {
+                                gameOver = true
+                                
+                            }
                         }
+                        
                         .buttonStyle(CustomButtonStyle())
+                        
                     }
                     CustomText(text: "Game Score: \(gameScore)")
-                    
+                    NavigationLink("How to Play", destination: InstructionsView())
+                        .font(Font.custom("Marker Felt", size: 24))
+                        .padding()
                     Spacer()
                 }
                 .padding()
             }
+            .alert(isPresented: $gameOver, content: {
+                Alert(title: Text("You won the game!"), dismissButton: .destructive(Text("Play again"), action: {
+                    withAnimation {
+                        gameScore = 0
+                        gameOver = false
+                    }
+                }))
+            })
         }
     }
     func endTurn()
